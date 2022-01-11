5302dc12791b71efd65bad6262c52290fcbf94d7
mehdibouaziz <mehdi.bouaziz@gadz.org>
2022-01-11 16:29:58 +0100

    updated landing w/ backgound and name card

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,17 +1,50 @@
 import React from 'react'
 import '../styles/landing.css';
-import Background from '../img/landing-background-dark.jpg'
+import { Typography } from '@mui/material';
+import { Transform } from '@mui/icons-material';
+import { lineHeight } from '@mui/system';
 
 
 function Landing() {
     return (
-        <div className='landing__div' style={{
-            backgroundImage: "url(" + Background + ")",
-            backgroundPosition: 'center',
-            backgroundSize: 'cover',
-            backgroundRepeat: 'no-repeat'
-            }}>
-            hello
+        <div className='landing__div'>
+            <div className="landing__card">
+                <Typography variant="subtitle1" className='landing__name' sx={{
+                    textAlign: 'center',
+                    fontSize: '0.8em',
+                    color: 'white',
+                    fontWeight: 100,
+                    fontVariant: 'small-caps',
+                    letterSpacing: '2px',
+                    lineHeight: 4,
+                }}>
+                    HELLO, I'M
+                </Typography>
+
+                <Typography variant="h1" className='landing__name' sx={{
+                    textAlign: 'center',
+                    fontSize: '4em',
+                    color: 'white',
+                    fontWeight: 500,
+                    letterSpacing: '3px',
+                    lineHeight: 1.5
+                }}>
+                    MEHDI BOUAZIZ
+                </Typography>
+
+                <Typography variant="subtitle1" className='landing__name' sx={{
+                    textAlign: 'center',
+                    fontSize: '2em',
+                    color: 'white',
+                    fontWeight: 200,
+                    letterSpacing: '-1px',
+                    lineHeight: 1
+                }}>
+                    FRONT-END DEVELOPER
+                </Typography>
+                
+
+            </div>
         </div>
     )
 }
