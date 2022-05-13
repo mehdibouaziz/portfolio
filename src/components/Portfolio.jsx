c74974cac95e3109680acef94977840e3ddf93ea
mehdibouaziz <mehdi.bouaziz@gadz.org>
2022-05-13 17:50:21 +0200

    changed recipe app description

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -18,7 +18,7 @@ const Portfolio = () => {
             img: 'recipeapp.png',
             alt: 'recipe app preview',
             name: 'Recipe App',
-            txt: 'Recipe catalog created with React for my own use. Includes a search engine and dark/light modes. WIP: JSON generator to add new recipes to the catalog',
+            txt: 'Recipe catalog created with React for my own use. Includes a search engine, dark/light modes and a JSON generator to add new recipes to the catalog.',
             links: [
                 ['https://mehdibouaziz.github.io/recipe-app/','github-pages','external','Github Pages'],
                 ['https://github.com/mehdibouaziz/recipe-app','github','github', 'Github repository']
@@ -29,7 +29,7 @@ const Portfolio = () => {
             img: 'wordle.png',
             alt: 'wordle app preview',
             name: 'Wordle Clone',
-            txt: 'Worlde clone created using React and Redux. Play with the word of the day or a random word. Can be played in English and French',
+            txt: 'Worlde clone created using React and Redux. Play with the word of the day or a random word. Can be played in English and French.',
             links: [
                 ['https://mehdibouaziz.github.io/wordle-clone/','github-pages','external','Github Pages'],
                 ['https://github.com/mehdibouaziz/wordle-clone','github','github', 'Github repository']
@@ -40,7 +40,7 @@ const Portfolio = () => {
             img: 'pomodoro.png',
             alt: 'pomodoro app preview',
             name: 'Pomodoro',
-            txt: '25/5 pomodoro clock created using React. Session and break duration can be set, remaining time in the session is vizualised with a color coded animated border',
+            txt: '25/5 pomodoro clock created using React. Session and break duration can be set, remaining time in the session is vizualised with a color coded animated border.',
             links: [
                 ['https://mehdibouaziz.github.io/pomodoro/','github-pages','external','Github Pages'],
                 ['https://github.com/mehdibouaziz/pomodoro','github','github', 'Github repository']
@@ -50,7 +50,7 @@ const Portfolio = () => {
             id: 'calculator',
             img: 'calculator.png',
             name: 'Calculator',
-            txt: 'Calculator created for FreeCodeCamp curriculum',
+            txt: 'Calculator created for the FreeCodeCamp curriculum.',
             links: [
                 ['https://mehdibouaziz.github.io/calculator-react/','github-pages','external','Github Pages'],
                 ['https://github.com/mehdibouaziz/calculator-react','github','github', 'Github repository']
@@ -60,7 +60,7 @@ const Portfolio = () => {
             id: 'quoteMachine',
             img: 'quoteMachine.png',
             name: 'Random Quote Machine',
-            txt: 'Random quote generator created for FreeCodeCamp curriculum. The quotes are provided by the Quotable API, with a fallback library of quotes in case of a fetch error',
+            txt: 'Random quote generator created for FreeCodeCamp curriculum. The quotes are provided by the Quotable API, with a fallback library of quotes in case of a fetch error.',
             links: [
                 ['https://mehdibouaziz.github.io/random-quote-machine/','github-pages','external','Github Pages'],
                 ['https://github.com/mehdibouaziz/random-quote-machine','github','github', 'Github repository']
