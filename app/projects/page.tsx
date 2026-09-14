2528328721d707f7b0fe896e1b3d58cff0db6bf0
mehdibouaziz <31729703+mehdibouaziz@users.noreply.github.com>
2026-09-15 01:07:40 +0200

    chore(core): remove projects page

--- app/projects/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function ProjectsPage() {
-    return (
-        <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-            <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-                PROJECTS
-            </main>
-        </div>
-    );
-}
