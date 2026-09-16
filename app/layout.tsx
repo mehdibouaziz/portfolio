84626548c2033af12d7cafd9e0aa9618dc26d580
mehdibouaziz <31729703+mehdibouaziz@users.noreply.github.com>
2026-09-16 13:49:06 +0200

    feat(core): add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type {Metadata} from "next";
 import {Geist, Geist_Mono} from "next/font/google";
 import "./globals.css";
 import ThemeProvider from "@/components/theme-provider/theme-provider";
+import { Analytics } from '@vercel/analytics/next';
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
@@ -33,6 +34,7 @@ export default function RootLayout({children}: LayoutProps<"/">) {
             disableTransitionOnChange
         >
             {children}
+            <Analytics />
         </ThemeProvider>
         </body>
         </html>
